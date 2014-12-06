e075e228a0ea95122c29e89a64d7d00228e9f88a
Chris Wand <chris.wand@gmail.com>
2014-12-06 15:09:19 -0500

    Working on markers

--- server.rb
+++ server.rb
@@ -12,20 +12,18 @@ end
 
 # holder for place to compare to
 def choose_place_name
-  'Somerville, Massachusetts'
+  ['Somerville, Massachusetts', 'Las Vegas, Nevada'].sample
 end
 
 # compares the distance between to lat-long points and returns miles between
-def compare_locations (user_select, comp_select)
-	puts "Your guess is #{user_select.distance_to(comp_select)} mile(s) off"
+def compare_locations(user_select, comp_select)
+  user_select.distance_to(comp_select).round
 end
 
 # assumes location is a physical address, and will return the lat-long of the location passed in
-def get_lat_long (location)
-		Geokit::Geocoders::GoogleGeocoder.geocode '#{location}'
+def get_lat_long(location)
+  Geokit::Geocoders::GoogleGeocoder.geocode "#{location}"
 end
-
-
 
 get '/' do
   @place_name = choose_place_name
@@ -33,8 +31,9 @@ get '/' do
   erb :'index'
 end
 
-
 get '/guess' do
-  # binding.pry
-  'You just guessed'
+  @place = get_lat_long(params['location'])
+  @guess = get_lat_long("#{params['lat']}, #{params['lng']}")
+  distance = compare_locations(@guess, @place).to_s
+  "#{@place.lat} #{@place.lng} #{distance}"
 end
